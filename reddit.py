1bdc786848694d955b64f4ec05e2237ae83d1aed
wrathinc <wrathinc@gmail.com>
2018-11-19 14:17:32 -0500

    Delete reddit.py

--- reddit.py
+++
@@ -1,4 +0,0 @@
-import praw 
-
-reddit = praw.Reddit(client_id='clientid', client_secret='secret', password='T0236346hd', useragent='PrawTut', username='u/tipsy3521')
-subreddit = reddit.subreddit('python')
