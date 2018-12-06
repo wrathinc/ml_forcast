a79ffa564fcf6e1c4d99fdec22bb79fa2e3290aa
wrathinc <git@wrathinc.com>
2018-12-05 22:15:30 -0500

    addind flare

--- detecting-blur/detect_blur.py
+++ detecting-blur/detect_blur.py
@@ -5,7 +5,8 @@
 from imutils import paths
 import argparse
 import cv2
-import os
+import os, time 
+from tqdm import tqdm
 
 def variance_of_laplacian(image):
     # compute the Laplacian of the image and then return the focus
@@ -19,26 +20,52 @@ ap.add_argument("-i", "--images", required=True,
 ap.add_argument("-t", "--threshold", type=float, default=100.0,
     help="focus measures that fall below this value will be considered 'blurry'")
 args = vars(ap.parse_args())
-print("starting...")
+print("The Sorting hat of blurry images!")
+
+print("""
+               __  _                          
+              / /_(_)___  _______  __         
+             / __/ / __ \/ ___/ / / /         
+            / /_/ / /_/ (__  ) /_/ /          
+ ___________\__/_/ .___/____/\__, /___________
+/_____/_____/   /_/         /____/_____/_____/
+""")
+
+
+
 
 # loop over the input images
 for imagePath in paths.list_images(args["images"]):
+    
     # load the image, convert it to grayscale, and compute the
     # focus measure of the image using the Variance of Laplacian
     # method
-    print("working...")
+    #print("The sorting hat is placed on the image!!")
     image = cv2.imread(imagePath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     fm = variance_of_laplacian(gray)
 
     if fm > args["threshold"]:
         cv2.imwrite("/home/mrrobot/Documents/ml_forcast/detecting-blur/notblurry/test{}.jpg".format(fm),image)
-        print("saving not blurry photos")
+        for image in tqdm(range(1)):
+            print("The Sorting hat says!!!! not blurry")
+        #print("The Sorting hat says!!!! not blurry")
    
     # if the focus measure is less than the supplied threshold,
     # then the image should be considered "blurry"
     if fm < args["threshold"]:
-        print("saving blurry")
+        #print("The Sorting hat says!!!! blurry")
         cv2.imwrite("/home/mrrobot/Documents/ml_forcast/detecting-blur/bp/test{}.jpg".format(fm),image)
+        for image in tqdm(range(1)):
+            print("The Sorting hat says!!!! blurry")
 
-print("done")
+    # for i in tqdm(range(len(imagePath))):
+    #     time.sleep(.1)
+print("""\n
+               __  _                          
+              / /_(_)___  _______  __         
+             / __/ / __ \/ ___/ / / /         
+            / /_/ / /_/ (__  ) /_/ /          
+ ___________\__/_/ .___/____/\__, /___________
+/_____/_____/   /_/         /____/_____/_____/\n
+""")
